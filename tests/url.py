5b4052fb53a05e0dffb39395ef44ab2af013c6f0
ptwt3d-creator <ptwt3d@gmail.com>
2026-09-06 18:49:20 +0300

    feat: add API endpoint constants

---
+++ tests/url.py
@@ -0,0 +1,9 @@
+
+
+class Url:
+
+    LOGIN_COURIER = "https://qa-scooter.praktikum-services.ru/api/v1/courier/login"
+    REGISTRATION_COURIER = "https://qa-scooter.praktikum-services.ru/api/v1/courier"
+    DELETE_COURIER = "https://qa-scooter.praktikum-services.ru/api/v1/courier/" 
+    ORDER_CREATE = "https://qa-scooter.praktikum-services.ru/api/v1/orders"
+    ORDER_GET_LIST = "https://qa-scooter.praktikum-services.ru/api/v1/orders"
