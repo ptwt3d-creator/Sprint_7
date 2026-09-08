97671a2126570cbd6903050d22d7797dc487fe08
ptwt3d-creator <ptwt3d@gmail.com>
2026-09-08 18:04:08 +0300

    refactor: adapt code for new file structure

--- tests/test_get_list_order.py
+++ tests/test_get_list_order.py
@@ -1,8 +1,9 @@
 import requests
-from testdata import TestData
-from url import Url
 import pytest
 import allure
+from data import TestUrl
+from api import ApiRequests
+
 
 
 @allure.suite("Проверки получения списка заказов")
@@ -11,30 +12,9 @@ class TestListOrder:
 
     @allure.title("Успешное получение списка всех заказов")
     @allure.description("Проверяем, что API возвращает список заказов, структура ответа корректна и содержит все обязательные поля.")
-    def test_get_list_order_returns_list_order(self):
-        TestData.order_create()
-        r = requests.get(Url.ORDER_GET_LIST)
-
-        expected_fields_orders = {
-        "id", 
-        "courierId", 
-        "firstName", 
-        "lastName", 
-        "address", 
-        "metroStation", 
-        "phone", 
-        "rentTime", 
-        "deliveryDate", 
-        "track", 
-        "color", 
-        "comment", 
-        "createdAt", 
-        "updatedAt", 
-        "status"
-        }
-
-        response_dict = r.json()
-        assert "orders" in response_dict and type(response_dict["orders"]) is list and len(response_dict["orders"]) > 0 and response_dict
+    def test_get_list_order_returns_list_order(self, create_order):
+        create_order
         
-        for field in expected_fields_orders:
-            assert field in response_dict["orders"][0], f"В заказе отсутствует поле: {field}"
+        r = ApiRequests.get_list_all_orders()
+
+        assert "orders" in r.json()
